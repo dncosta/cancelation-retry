b38812c97294c295a925ae549d94f6fc9f2240b8
dncosta <diegonunescosta@gmail.com>
2021-11-26 19:25:35 -0300

    Including search for non-concluded refunds in metabase's API

--- pags-cancel.py
+++ pags-cancel.py
@@ -2,57 +2,101 @@ import requests
 import json
 import time
 
-charges = ["aaaaa", "bbbbb", "cccc"]
-amounts = [1, 2, 3]
-token = 'Bearer token'
+# sets requests variables for Metabase and PagSeguro
 
-for charge, amount in zip(charges, amounts):
+url_session = "https://metabase.moip.com.br/api/session"
+url_refunds = "https://metabase.moip.com.br/api/card/10697/query"
+url_pagseguro = "https://api.pagseguro.com/charges/"
+username = # PLACE USERNAME
+password = # PLACE USERNAME
 
-	url = "https://api.pagseguro.com/charges/" + str(charge)
+# starts metabase session
 
-	payload={}
-	headers = {
-	  'Authorization': token,
-	  'Content-Type': 'application/json',
-	  'x-api-version': '4.0'
-	}
+print("Starting new session")
 
-	response = requests.request("GET", url, headers=headers, data=payload)
-	http_status = response.status_code
-	response_json = json.loads(response.text)
+payload = json.dumps({
+  "username": username ,
+  "password": password
+})
+headers = {
+  'Content-Type': 'application/json',
+}
 
-	if(http_status == 200):
-			charge_status = response_json['status']
-			refunded_amount = response_json['amount']['summary']['refunded']
-			missing = amount - refunded_amount
-			
-			if(missing > 0):
-				url = "https://api.pagseguro.com/charges/" + str(charge) + "/cancel"
-				payload = json.dumps({
-					"amount": { 
-					"value": missing
-					}
-				})
-				headers = {
-				  'Authorization': token,
-				  'Content-Type': 'application/json',
-				  'X-api-version': '4.0'
-				}
-				response = requests.request("POST", url, headers=headers, data=payload)
-				http_status = response.status_code
-				response_json = json.loads(response.text)
+response_session = requests.request("POST", url_session, headers=headers, data=payload)
+response_dict = json.loads(response_session.text)
+session_id = response_dict['id']
 
-				if((http_status == 200) or (http_status == 201)):
-					charge_status = response_json['status']
-					refunded_amount = response_json['amount']['summary']['refunded']
-					print("POST-cancel" + charge + ";" + charge_status + ";" + str(refunded_amount) + ";" + str(amount) + ";" + str(http_status) + ";" + "" + ";" + str(missing))
-				else:
-					error = response_json['error_messages']
-					print("POST-cancel-error" + charge + ";" + "" + ";" + "" + ";" + "" + ";" + str(http_status) + ";" + str(error) +";" + "")
-				time.sleep(1)
-			else:
-				print("GET-refund" +charge + ";" + charge_status + ";" + str(refunded_amount) + ";" + str(amount) + ";" + str(http_status) + ";" + str(missing))
+print("New session started: " + session_id)
 
-	else:
-		print("GET-refund-error" + "," + charge + ";" + "" + ";" + "" + ";" + "" + ";" + str(http_status))
-	time.sleep(1)
+# retrieves the pending refunds from metabase and creates a list of charges
+
+print("Getting pending refunds list")
+
+payload = json.dumps({
+  "ignore_cache": False,
+  "parameters": []
+})
+headers = {
+  'X-Metabase-Session': session_id,
+  'Content-Type': 'application/json',
+  'Cookie': 'metabase.SESSION='+session_id
+}
+
+response_refunds = requests.request("POST", url_refunds, headers=headers, data=payload)
+refunds_dict = json.loads(response_refunds.text)
+refund_row = refunds_dict['data']['rows']
+
+charges = [item[0] for item in refund_row]
+payments = [item[1] for item in refund_row]
+amounts = [int(item[3]) for item in refund_row]
+affiliations = [item[4] for item in refund_row]
+
+print("Starting the refunds in PagSeguro")
+
+# processes the refunds in PagSeguro
+
+for charge, amount, affiliation in zip(charges, amounts, affiliations):
+
+  payload={}
+  headers = {
+    'Authorization': "Bearer " + affiliation,
+    'Content-Type': 'application/json',
+    'x-api-version': '4.0'
+  }
+
+  response = requests.request("GET", url_pagseguro + str(charge), headers=headers, data=payload)
+  http_status = response.status_code
+  response_json = json.loads(response.text)
+
+  if(http_status == 200):
+      charge_status = response_json['status']
+      refunded_amount = response_json['amount']['summary']['refunded']
+      missing = amount - refunded_amount
+
+      if(missing > 0):
+        payload = json.dumps({
+          "amount": { 
+          "value": missing
+          }
+        })
+        headers = {
+          'Authorization': "Bearer " + affiliation,
+          'Content-Type': 'application/json',
+          'X-api-version': '4.0'
+        }
+        response = requests.request("POST", url_pagseguro + str(charge) + "/cancel", headers=headers, data=payload)
+        http_status = response.status_code
+        response_json = json.loads(response.text)
+
+        if((http_status == 200) or (http_status == 201)):
+          charge_status = response_json['status']
+          refunded_amount = response_json['amount']['summary']['refunded']
+          print("POST-cancel; ID:" + charge + ";STATUS: " + charge_status + "; REFUNDED_AMOUNT: " + str(refunded_amount) + "; TOTAL: " + str(amount) + "; HTTP_STATUS: " + str(http_status) + ";" + "" + ";MISSING: " + str(missing))
+        else:
+          error = response_json['error_messages']
+          print("POST-cancel-error; ID: " + charge + ";" + "" + ";" + "" + ";" + "" + ";" + str(http_status) + ";" + str(error) +";" + "")
+      else:
+        print("GET-refund; ID: " +charge + ";STATUS: " + charge_status + "; REFUNDED_AMOUNT: " + str(refunded_amount) + "; TOTAL: " + str(amount) + "; HTTP_STATUS: " + str(http_status) + ";MISSING: " + str(missing))
+
+  else:
+    print("GET-refund-error; ID: " + ";" + charge + ";" + "" + ";" + "" + ";" + "" + ";" + str(http_status))
